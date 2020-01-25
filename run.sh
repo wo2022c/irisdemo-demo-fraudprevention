6eaa896d221d4061e400bfeddec74d1e5b94e3ee
Amir Samary <amirsamary@gmail.com>
2020-01-25 10:21:16 -0500

    Improving run.sh command

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+RESET="\033[0m"
+
+cleanup()
+{
+    printf "\n\n${PURPLE}CTRL+C detected. Removing containters...${RESET}\n"
+    docker-compose stop
+    docker-compose rm -f
+
+    printf "\n\n${PURPLE}Cleaning up complete.${RESET}\n"
+    trap - INT
+}
+trap cleanup INT
 
 printf "\n\nStopping and deleting previous containers...\n"
 docker-compose stop
